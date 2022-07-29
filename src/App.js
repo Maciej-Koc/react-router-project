4a8f257a4a71dba793157304d1ca4a357a10d77f
Maciej-Koc <mkoc2@asu.edu>
2022-07-28 22:52:09 -0500

    Enabled routes for products using Switch and exact prop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Welcome from "./Welcome";
 import Products from "./Products";
 import MainHeader from "./MainHeader";
+import ProductDetail from "./ProductDetail";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/Products.js
+++ src/Products.js
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Products() {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="products/p2">Water</Link>
+        </li>
+        <li>
+          <Link to="products/p3">Pencil</Link>
+        </li>
+      </ul>
+    </section>
+  );
 }
