ff39a72bc5a0d6ce86d60d7f622acf82fca7baec
Maciej-Koc <mkoc2@asu.edu>
2022-07-28 20:35:44 -0500

    Initial commit with routes for products and welcome complete.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import Products from "./Products";
+
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
